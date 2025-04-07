045399e049511e6c383afbc69d217aa0e3f3d590
kelian-hub <kelian.demoura@external.exxelia.com>
2025-04-07 10:39:38 +0200

    voila

--- test.php
+++ test.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Test pour github et php</title>
 </head>
 <body>
     <?php
-echo 'voila github'
+echo 'voila php fonctionne et github relier a mon vscode'
 ?>
 </body>
 </html>
